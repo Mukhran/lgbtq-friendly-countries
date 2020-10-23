4581fba7452cab874d202ab014538a68ba952634
Marina Mukhranskaia <mukhranski@gmail.com>
2020-10-23 16:59:55 +0000

    rejected route was fixed, country_filter added

--- server.py
+++ server.py
@@ -4,16 +4,20 @@ from flask import Flask, render_template, request, redirect, url_for
 from model import db, Country, Link, Review, connect_to_db
 # from sqlalchemy.orm import sessionmaker
 from jinja2 import StrictUndefined
-
+from serverkey import SUPERSECRETSERVERKEY
 
 
 
 app = Flask(__name__)
 
+
+
 @ app.route('/')
 def index():
     """Homepage"""
     
+    country_filter=request.args.get("country_filter")
+    print(country_filter,'#####################')
     item = db.session.query(Country.country_name, Link.link).join(Link).all()
 
     # revs = Review.query.filter_by(moderation_status="approved").all()
@@ -23,41 +27,46 @@ def index():
         Review.score
     ).filter_by(moderation_status='approved').all()
 
+
     
     return render_template("index.html", item=item, rev=rev) 
     
 
 @ app.route('/moderation/approved/<id>', methods=["POST", "GET"])
 def update_status(id):
-    
-
     id = Review.query.get(id)
     id.moderation_status = 'approved'
     db.session.commit()
 
-    print(id)
-
-    # return redirect(url_for("approved"))
     return render_template("approved.html", id=id)
 
-@ app.route('/moderation/rejected/<id>', methods=["POST, GET"])
+@ app.route('/moderation/rejected/<id>', methods=["POST", "GET"])
 def reject_status(id):
     id = Review.query.get(id)
     id.moderation_status = 'rejected'
     db.session.commit()
+    print(id)
 
-    return f" Rejected"
+    return render_template("rejected.html", id=id)
 
 
 @app.route('/submitreview', methods=['POST', 'GET'])
 def submit_review():
     text= request.form.get("review")
     
-    country=request.form.get("country")
     if text is not None:
-        r = Review(moderation_status="pending")
+        country=Country.query.filter_by(country_name=request.form.get("country")).first()
+        
+        country_id=request.form.get("country")
+        
+        score=request.form.get("score")
+        
 
-        return redirect('/', code=302)
+        r=Review(text=text, score=score, moderation_status='pending', country_id=country_id)
+        
+        db.session.add(r)
+        db.session.commit()
+        return redirect('/')
 
     item = db.session.query(Country.id, Country.country_name).all()
     return render_template('submitreview.html', item=item)
